8a74db580528666db56a4cfc77308c421bedb0e6
guilherme.castilho <guilherme.castilho@semantix.com.br>
2022-06-26 14:57:35 -0300

    topic-url-id_added

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # página inicial
     url(r'^$', views.index, name='index'),
     url(r"^topics/$", views.topics, name='topics'),
+    # página de detalhes para um único assunto
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]

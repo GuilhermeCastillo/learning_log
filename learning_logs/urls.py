0160cb68cbf4d4164f88900de41895ab6099d61f
guilherme.castilho <guilherme.castilho@semantix.com.br>
2022-06-26 16:23:05 -0300

    rota-adicionar-entrada_adicionada

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     # Página para adicionar um novo assunto
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # Página para adicionar uma nova entrada
+    url(r'^new_entry/(?P<topic_id>\d+)', views.new_entry, name='new_entry')
 ]
